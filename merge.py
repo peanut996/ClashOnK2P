db324635bad6432d3131853bd9bdbecc8d33f75c
Peanuts <849421294godw@gmail.com>
2022-04-12 12:06:32 +0000

    支持自定义规则

--- merge.py
+++ merge.py
@@ -1,6 +1,11 @@
 import yaml
 from collections import OrderedDict
 
+PERSONAL_RULE = [
+    "DOMAIN-KEYWORD,807.91p51.com,DIRECT",
+    "DOMAIN-KEYWORD,killcovid2021,DIRECT"
+]
+
 
 def ordered_yaml_load(stream, Loader=yaml.SafeLoader,
                       object_pairs_hook=OrderedDict):
@@ -31,6 +36,13 @@ def ordered_yaml_dump(data, stream=None, Dumper=yaml.SafeDumper,
     return yaml.dump(data, stream, OrderedDumper, **kwds)
 
 
+def edit_rule(rules):
+    last_rule = rules.pop()
+    rules += PERSONAL_RULE
+    rules.append(last_rule)
+    return rules
+
+
 if __name__ == "__main__":
     file = open("base.yaml", "r", encoding="utf-8")
     config = ordered_yaml_load(file)
@@ -39,7 +51,7 @@ if __name__ == "__main__":
 
     config["proxies"] = bk_config["proxies"]
     config["proxy-groups"] = bk_config["proxy-groups"]
-    config["rules"] = bk_config["rules"]
+    config["rules"] = edit_rule(bk_config["rules"])
 
     with open("config.yaml", "w", encoding="utf-8") as f:
         f.write(ordered_yaml_dump(config, allow_unicode=True, encoding=None))
