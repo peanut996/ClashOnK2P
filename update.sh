92d69e87c3a905862cf4442796cce8e1228a748a
Peanuts <849421294godw@gmail.com>
2023-02-26 07:37:10 +0000

    update workflow

---
+++ update.sh
@@ -0,0 +1,7 @@
+REPO="Dreamacro/clash"
+TAG=$(curl -s "https://api.github.com/repos/$REPO/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+ASSET_URL=$(curl -s "https://api.github.com/repos/$REPO/releases/tags/$TAG" | jq -r '.assets[] | select(.name | test(" clash-linux-mipsle-hardfloat")) | .browser_download_url')
+echo "Downloading $ASSET_URL"
+curl -L -o clash.gz "$ASSET_URL"
+gzip -d clash.gz
+mv clash binary/
